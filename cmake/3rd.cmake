2b32282a7cac5177da04a69cbf5a77f887b5a4d7
caodhuan <cao.d.huan@gmail.com>
2017-11-16 11:17:09 +0800

    添加protobuf

---
+++ cmake/3rd.cmake
@@ -0,0 +1,23 @@
+# 添加protobuf
+file(GLOB pb ${CMAKE_CURRENT_SOURCE_DIR}/3rd/protobuf/cmake/*)
+if(pb)
+	message(STATUS "found protobuf")
+	option(protobuf_BUILD_TESTS OFF)
+
+	add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/3rd/protobuf/cmake/)
+	
+	include_directories(${CMAKE_CURRENT_SOURCE_DIR}/3rd/protobuf/src)
+
+	set_target_properties(libprotobuf PROPERTIES FOLDER 3rd/protobuf)
+	set_target_properties(libprotobuf-lite PROPERTIES FOLDER 3rd/protobuf)
+	set_target_properties(libprotoc PROPERTIES FOLDER 3rd/protobuf)
+	set_target_properties(protoc PROPERTIES FOLDER 3rd/protobuf)
+	set_target_properties(js_embed PROPERTIES FOLDER 3rd/protobuf)
+
+else()
+	message("CANT find protobuf")
+endif()
+
+
+
+add_subdirectory(3rd)
