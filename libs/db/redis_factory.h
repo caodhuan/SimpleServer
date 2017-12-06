6eaf2b3a73a1dd08920216758ca394db811ceafc
caodhuan <cao.d.huan@gmail.com>
2017-12-06 14:52:51 +0800

    单例支持继承

--- libs/db/redis_factory.h
+++ libs/db/redis_factory.h
@@ -8,7 +8,7 @@ namespace CHServer {
 
 	// 异步的redis需要new出来，不允许在栈上创建
 	// 干脆做个统一的工厂方法了
-	class RedisFactory : public SingletonBase<RedisFactory> {
+	class RedisFactory : public Singleton<RedisFactory> {
 	public:
 		RedisAsync* CreateRedisAsync();
 		RedisSync* CreateRedisSync();
