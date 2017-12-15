52b4e263efd45d8d10723f716042c6a9cdc1f191
caodhuan <cao.d.huan@gmail.com>
2017-12-15 11:10:03 +0800

    读取lua配置实现

--- libs/resource/config.cpp
+++ libs/resource/config.cpp
@@ -1,14 +1,97 @@
 #include "config.h"
+#include "log.h"
 
 namespace CHServer {
-	
-	Config::Config(const char* path) {
+
+	static int Panic(lua_State* pState) {
+		const char *msg = lua_tostring(pState, -1);
+
+		CHERRORLOG(msg);
+
+		return 0;
+	}
+
+	Config::Config(const char* path, const char* tableName)
+		: m_state(nullptr)
+		, m_haveParent(false) {
+
 		m_state = luaL_newstate();
+
+		lua_atpanic(m_state, Panic);
+		luaL_openlibs(m_state);
+
+		luaL_dofile(m_state, path);
+
+		AddTableName(tableName);
+	}
+
+	Config::Config(lua_State* state)
+		: m_state(state)
+		, m_haveParent(true) {
 
 	}
 
 	Config::~Config() {
+		if (!m_haveParent && m_state) {
+			lua_close(m_state);
+			m_state = nullptr;
+		}
+	}
 
+	bool Config::ReadInt(const char* name, int& result) {
+		PreparaStack(name);
+		if (!lua_isinteger(m_state, -1)) {
+			return false;
+		}
+
+		result = lua_tointeger(m_state, -1);
+
+		return true;
+	}
+
+	bool Config::ReadString(const char* name, std::string& result) {
+		PreparaStack(name);
+
+		if (!lua_isstring(m_state, -1)) {
+			return false;
+		}
+
+		size_t size = 0;
+		const char* str = lua_tolstring(m_state, -1, &size);
+		result.assign(str, size);
+		return true;
+	}
+
+	Config* Config::ReadTable(const char* name) {
+		PreparaStack(name);
+		if (!lua_istable(m_state, -1)) {
+			return nullptr;
+		}
+
+		Config *config = new Config(m_state);
+
+		config->AddTableName(name);
+
+		return config;
+	}
+
+	void Config::AddTableName(const char* name) {
+		m_tableName.push_back(name);
+	}
+
+	void Config::PreparaStack(const char* fieldName) {
+		lua_getglobal(m_state, m_tableName[0].c_str());
+
+		int32_t deepth = m_tableName.size();
+		for (int i = 1; i < deepth; ++i) {
+			lua_pushstring(m_state, m_tableName[i].c_str());
+			if (i < deepth - 1) {
+				lua_rawget(m_state, -2);
+			}
+		}
+
+		lua_pushstring(m_state, fieldName);
+		lua_rawget(m_state, -2);
 	}
 
 }
--- libs/resource/config.h
+++ libs/resource/config.h
@@ -1,17 +1,37 @@
 #pragma once
 
 #include <lua.hpp>
-
+#include <string>
+#include <vector>
 namespace CHServer {
 	// 读取服务器配置
+	// 一个Config代表一组配置，每组配置用table来表示
+	// TODO: 索引表该如何读出？
 	class Config {
 	public:
-		Config(const char* path);
+		Config(const char* path, const char* tableName);
 		~Config();
-	public:
+	private:
+		Config(lua_State* state);
 
+	public:
+		bool ReadInt(const char* name, int& result);
+		
+		bool ReadString(const char* name, std::string& result);
+
+		// 这个Config与父Config共享一个虚拟机
+		Config* ReadTable(const char* name);
+
+	private:
+		void AddTableName(const char* name);
+
+		void PreparaStack(const char* fieldName);
+		
 	private:
 		// 每个配置文件，单独一个虚拟机
 		lua_State* m_state;
+		bool m_haveParent;
+
+		std::vector<std::string> m_tableName;
 	};
 }
