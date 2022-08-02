5ad12a5fab69e1953e78994aeeaf9c601d86c4c0
caodhuan <cao.d.huan@gmail.com>
2022-08-02 15:38:39 +0800

    misc

--- src/test/client_test/main.cpp
+++ src/test/client_test/main.cpp
@@ -1,167 +1,163 @@
 #ifdef WIN32
 #define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
 #include <crtdbg.h>
+#include <stdlib.h>
 #endif
 
-#include "socket_tcp.h"
-#include "event_dispatcher.h"
-
-#include "uv.h"
-#include "mysql.h"
-#include "log.h"
+#include <string.h>
 
 #include <iostream>
-#include <string.h>
-#include "session.h"
-#include "common.pb.h"
 
+#include "commonprotobuf.h"
+#include "event_dispatcher.h"
+#include "log.h"
+#include "mysql.h"
+#include "session.h"
+#include "socket_tcp.h"
 
 using namespace std;
 using namespace CHServer;
 
 void TestMysql() {
-	cout << "test mysql" << endl;
-	MYSQL* mysql = mysql_init(NULL);
+  cout << "test mysql" << endl;
+  MYSQL* mysql = mysql_init(NULL);
 
-	if (mysql_real_connect(mysql, "127.0.0.1", "x", "x", "x", 3306, NULL, 0)) {
-		if (mysql_select_db(mysql, "x") == 0) {
+  if (mysql_real_connect(mysql, "127.0.0.1", "x", "x", "x", 3306, NULL, 0)) {
+    if (mysql_select_db(mysql, "x") == 0) {
+      char cmd[] = "select * from tbl_accounts";
+      int nResult = mysql_real_query(mysql, cmd, strlen(cmd));
+      if (nResult == 0) {
+        MYSQL_RES* res = mysql_store_result(mysql);
+        cout << res->row_count << endl;
+      } else {
+        cout << "error" << endl;
+      }
+    }
+  }
 
-			char cmd[] = "select * from tbl_accounts";
-			int nResult = mysql_real_query(mysql, cmd, strlen(cmd));
-			if (nResult == 0) {
-				MYSQL_RES* res = mysql_store_result(mysql);
-				cout << res->row_count << endl;
-			} else {
-				cout << "error" << endl;
-			}
-		}
-	}
-
-	mysql_close(mysql);
+  mysql_close(mysql);
 }
 
-
 void TestLibuv() {
+  EventDispatcher* dispatcher = new EventDispatcher();
+  SocketTCP* client = new SocketTCP(dispatcher);
+  bool readOnce = false;
+  char msg[] = "12312312";
+  std::string str;
+  str.assign(msg);
 
-	EventDispatcher* dispatcher = new EventDispatcher();
-	SocketTCP* client = new SocketTCP(dispatcher);
-	bool readOnce = false;
-	char msg[] = "12312312";
-	std::string str;
-	str.assign(msg);
+  client->SetCallback(
+      [&] {
+        client->Send(msg, sizeof(msg));
+        // client->Close();
+      },
+      [&]() -> void {
+        int len = client->GetDataLength();
+        char* ptr = NULL;
 
-	client->SetCallback([&] {
+        if (!readOnce && len > 1025) {
+          readOnce = true;
+          client->RemoveBuff(1025);
+        }
+        int readLen = client->ReadBuff(ptr);
 
-		client->Send(msg, sizeof(msg));
-		//client->Close();
-	}, [&]()-> void {
-		int len = client->GetDataLength();
-		char* ptr = NULL;
+        std::string tmp;
+        tmp.assign(ptr, readLen);
+        CHERRORLOG(tmp.c_str());
+        //
+        // 		if (readLen < len) {
+        // 			client->RemoveBuff(readLen);
+        // 			readLen = client->ReadBuff(ptr);
+        // 			str.append(ptr, readLen);
+        // 		}
+        // 		client->RemoveBuff(readLen);
 
-		if (!readOnce && len > 1025) {
-			readOnce = true;
-			client->RemoveBuff(1025);
-		}
-		int readLen = client->ReadBuff(ptr);
+        // std::cout << str.c_str();
 
-		std::string tmp;
-		tmp.assign(ptr, readLen);
-		CHERRORLOG(tmp.c_str());
-		// 
-		// 		if (readLen < len) {
-		// 			client->RemoveBuff(readLen);
-		// 			readLen = client->ReadBuff(ptr);
-		// 			str.append(ptr, readLen);
-		// 		}
-		// 		client->RemoveBuff(readLen);
+        client->Send(str.c_str(), str.size());
+      },
+      nullptr);
 
-		//std::cout << str.c_str();
-
-		client->Send(str.c_str(), str.size());
-	}, nullptr);
-
-	//client->Connect("139.199.199.121", 2345);
-	client->Connect("127.0.0.1", 2345);
-	dispatcher->Run();
+  // client->Connect("139.199.199.121", 2345);
+  client->Connect("127.0.0.1", 2345);
+  dispatcher->Run();
 }
 
 void TestLibuv100000() {
+  EventDispatcher* dispatcher = new EventDispatcher();
 
-	EventDispatcher* dispatcher = new EventDispatcher();
+  for (int i = 0; i < 0; ++i) {
+    SocketTCP* client = new SocketTCP(dispatcher);
 
-	for (int i = 0; i < 0; ++i) {
-		SocketTCP* client = new SocketTCP(dispatcher);
+    // 		client->SetCallback([&] {
+    // 			std::cout << "connected" << std::endl;
+    // 			client->Send(msg, sizeof(msg));
+    // 		}, [&]()-> void {
+    // 			int len = client->GetBuffLength();
+    // 			char* ptr = NULL;
+    //
+    // 			if (!readOnce && len > 1025) {
+    // 				readOnce = true;
+    // 				client->RemoveBuff(1025);
+    // 			}
+    // 			int readLen = client->ReadBuff(ptr);
+    //
+    // 			std::string tmp;
+    // 			tmp.assign(ptr, readLen);
+    // 			CHERRORLOG(tmp.c_str());
+    // 			//
+    // 			// 		if (readLen < len) {
+    // 			// 			client->RemoveBuff(readLen);
+    // 			// 			readLen = client->ReadBuff(ptr);
+    // 			// 			str.append(ptr, readLen);
+    // 			// 		}
+    // 			// 		client->RemoveBuff(readLen);
+    //
+    // 			//std::cout << str.c_str();
+    //
+    // 			//client->Send(str.c_str(), str.size());
+    // 		});
 
+    // client->Connect("127.0.0.1", 2345);
+    client->Connect("127.0.0.1", 2345);
+  }
+  std::cout << "connected" << std::endl;
 
-		// 		client->SetCallback([&] {
-		// 			std::cout << "connected" << std::endl;
-		// 			client->Send(msg, sizeof(msg));
-		// 		}, [&]()-> void {
-		// 			int len = client->GetBuffLength();
-		// 			char* ptr = NULL;
-		// 
-		// 			if (!readOnce && len > 1025) {
-		// 				readOnce = true;
-		// 				client->RemoveBuff(1025);
-		// 			}
-		// 			int readLen = client->ReadBuff(ptr);
-		// 
-		// 			std::string tmp;
-		// 			tmp.assign(ptr, readLen);
-		// 			CHERRORLOG(tmp.c_str());
-		// 			// 
-		// 			// 		if (readLen < len) {
-		// 			// 			client->RemoveBuff(readLen);
-		// 			// 			readLen = client->ReadBuff(ptr);
-		// 			// 			str.append(ptr, readLen);
-		// 			// 		}
-		// 			// 		client->RemoveBuff(readLen);
-		// 
-		// 			//std::cout << str.c_str();
-		// 
-		// 			//client->Send(str.c_str(), str.size());
-		// 		});
+  dispatcher->Run();
 
-		//client->Connect("127.0.0.1", 2345);
-		client->Connect("127.0.0.1", 2345);
-
-	}
-	std::cout << "connected" << std::endl;
-
-	dispatcher->Run();
-
-	delete dispatcher;
+  delete dispatcher;
 }
 
 void TestSession() {
-	EventDispatcher* dispatcher = new EventDispatcher();
-	SocketTCP* tcp = new SocketTCP(dispatcher);
-	Session* serve = new Session(tcp);
-	tcp->SetCallback([&] {
-		PlayerInfo info;
-		info.set_name("test name");
-		std::string data = info.SerializeAsString();
-		serve->SendPacket(1, data.c_str(), data.size());
-	}, nullptr, nullptr);
+  EventDispatcher* dispatcher = new EventDispatcher();
+  SocketTCP* tcp = new SocketTCP(dispatcher);
+  Session* serve = new Session(tcp);
+  tcp->SetCallback(
+      [&] {
+        ss::PlayerInfo info;
+        info.set_name("test name");
+        std::string data = info.SerializeAsString();
+        serve->SendPacket(1, data.c_str(), data.size());
+      },
+      nullptr, nullptr);
 
-	tcp->Connect("127.0.0.1", 2345);
+  tcp->Connect("127.0.0.1", 2345);
 
-	dispatcher->Run();
+  dispatcher->Run();
 }
 int main() {
 #ifdef WIN32
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
+  _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+  _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
 #endif
-	CHLog::Instance()->InitLog(".", "ch");
+  CHLog::Instance()->InitLog(".", "ch");
 
-	TestSession();
-	//TestMysql();
-	//TestLibuv();
-	//TestLibuv100000();
-	//char* test = new char[1000];
-	//_CrtDumpMemoryLeaks();
+  TestSession();
+  // TestMysql();
+  // TestLibuv();
+  // TestLibuv100000();
+  // char* test = new char[1000];
+  //_CrtDumpMemoryLeaks();
 
-	CHLog::Instance()->UninitLog();
+  CHLog::Instance()->UninitLog();
 }
